61a3af49856566a7944b8702c3f23b535e11cd31
Mirion <51433180+MirionQs@users.noreply.github.com>
2024-03-26 13:11:23 +0800

    impl part of bigint

--- bigint.h
+++ bigint.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#include <iostream>
+#include <bit>
 #include <array>
+#include <vector>
+#include <string_view>
 
 namespace plastic {
 
@@ -15,7 +17,7 @@ namespace plastic {
 		data_type _data;
 
 		static ::std::pair<::std::uint64_t, ::std::uint64_t> _mul(::std::uint64_t num1, ::std::uint64_t num2) {
-#ifdef _MSC_VER
+#if defined(_MSC_VER) && defined(_WIN64)
 			::std::uint64_t high, low;
 			low = _umul128(num1, num2, &high);
 			return {high, low};
@@ -31,6 +33,16 @@ namespace plastic {
 				x3 += 0x100000000;
 			}
 			return {x3 + (x1 >> 32), (x1 << 32) + (x0 & 0xffffffff)};
+#endif
+		}
+
+		static ::std::pair<::std::uint64_t, ::std::uint64_t> _div(::std::uint64_t num1High, ::std::uint64_t num1Low, ::std::uint64_t num2) {
+#if defined(_MSC_VER) && defined(_WIN64)
+			::std::uint64_t quot, rem;
+			quot = _udiv128(num1High, num1Low, num2, &rem);
+			return {quot, rem};
+#else
+			TODO
 #endif
 		}
 
@@ -196,8 +208,8 @@ namespace plastic {
 			_data.emplace_back(_sign > 0 ? num : -num);
 		}
 
-		explicit bigint(::std::string_view num, int base = 0) {
-			if (base < 0 || base == 1 || base > 36 || num.empty()) {
+		explicit bigint(::std::string_view num, unsigned base = 0) {
+			if (base == 1 || base > 36 || num.empty()) {
 				::std::abort();
 			}
 
@@ -266,25 +278,25 @@ namespace plastic {
 			};
 
 			/*
-				Table[StringJoin["0x", IntegerString[If[IntegerQ[Log2[b]], Log2[b], Floor[2^61*Log2[b]]], 16, 16]], {b, 2, 36}]
+				Table[StringJoin["0x", IntegerString[If[IntegerQ[Log2[b]], Log2[b], Ceiling[2^61*Log2[b]]], 16, 16]], {b, 2, 36}]
 				Table[If[IntegerQ[Log2[b]], 0, Floor[Log[b, 2^64]], 0], {b, 2, 36}]
 				Table[StringJoin["0x", IntegerString[If[IntegerQ[Log2[b]], 0, b^Floor[Log[b, 2^64]]], 16, 16]], {b, 2, 36}]
 			*/
 
-			static constexpr ::std::array<::std::uint64_t, 37> log2BaseTable{
-				0x0000000000000000, 0x0000000000000000, 0x0000000000000001, 0x32b803473f7ad0f3,
-				0x0000000000000002, 0x4a4d3c25e68dc57f, 0x52b803473f7ad0f3, 0x59d5d9fd5010b366,
-				0x0000000000000003, 0x6570068e7ef5a1e7, 0x6a4d3c25e68dc57f, 0x6eb3a9f01975077f,
-				0x72b803473f7ad0f3, 0x766a008e4788cbcd, 0x79d5d9fd5010b366, 0x7d053f6d26089673,
-				0x0000000000000004, 0x82cc7edf592262cf, 0x8570068e7ef5a1e7, 0x87ef05ae409a0288,
-				0x8a4d3c25e68dc57f, 0x8c8ddd448f8b845a, 0x8eb3a9f01975077f, 0x90c10500d63aa658,
-				0x92b803473f7ad0f3, 0x949a784bcd1b8afe, 0x966a008e4788cbcd, 0x982809d5be7072db,
-				0x99d5d9fd5010b366, 0x9b74948f5532da4b, 0x9d053f6d26089673, 0x9e88c6b3626a72aa,
-				0x0000000000000005, 0xa16bad3758efd873, 0xa2cc7edf592262cf, 0xa4231623369e78e5,
-				0xa570068e7ef5a1e7
+			static constexpr ::std::array<::std::uint64_t, 37> logTable{
+				0x0000000000000000, 0x0000000000000000, 0x0000000000000001, 0x32b803473f7ad0f4,
+				0x0000000000000002, 0x4a4d3c25e68dc580, 0x52b803473f7ad0f4, 0x59d5d9fd5010b367,
+				0x0000000000000003, 0x6570068e7ef5a1e8, 0x6a4d3c25e68dc580, 0x6eb3a9f019750780,
+				0x72b803473f7ad0f4, 0x766a008e4788cbce, 0x79d5d9fd5010b367, 0x7d053f6d26089674,
+				0x0000000000000004, 0x82cc7edf592262d0, 0x8570068e7ef5a1e8, 0x87ef05ae409a0289,
+				0x8a4d3c25e68dc580, 0x8c8ddd448f8b845b, 0x8eb3a9f019750780, 0x90c10500d63aa659,
+				0x92b803473f7ad0f4, 0x949a784bcd1b8aff, 0x966a008e4788cbce, 0x982809d5be7072dc,
+				0x99d5d9fd5010b367, 0x9b74948f5532da4c, 0x9d053f6d26089674, 0x9e88c6b3626a72ab,
+				0x0000000000000005, 0xa16bad3758efd874, 0xa2cc7edf592262d0, 0xa4231623369e78e6,
+				0xa570068e7ef5a1e8
 			};
 
-			static constexpr ::std::array<int, 37> chunkSizeTable{
+			static constexpr ::std::array<::std::size_t, 37> chunkSizeTable{
 				0, 0, 0, 40, 0, 27, 24, 22, 0, 20, 19, 18, 17, 17, 16, 16,
 				0, 15, 15, 15, 14, 14, 14, 14, 13, 13, 13, 13, 13, 13, 13, 12, 0, 12, 12, 12,
 				12
@@ -303,13 +315,13 @@ namespace plastic {
 				0x41c21cb8e1000000
 			};
 
-			if (::std::has_single_bit(static_cast<unsigned>(base))) {
-				int bits{static_cast<int>(log2BaseTable[base])};
+			if (::std::has_single_bit(base)) {
+				::std::size_t baseBits{logTable[base]};
 
-				_data.resize(((end - begin) * bits >> 6) + 1);
+				_data.resize(((end - begin) * baseBits >> 6) + 1);
 				auto dBegin{_data.begin()}, dEnd{dBegin};
 
-				int offset{0};
+				::std::size_t offset{0};
 				::std::uint64_t value{0};
 				while (begin != end) {
 					::std::uint8_t digit{fromCharTable[static_cast<::std::uint8_t>(*--end)]};
@@ -317,11 +329,11 @@ namespace plastic {
 						::std::abort();
 					}
 					value |= ::std::uint64_t{digit} << offset;
-					offset += bits;
+					offset += baseBits;
 					if (offset >= 64) {
 						*dEnd++ = value;
-						offset -= 64;
-						value = digit >> (bits - offset);
+						offset &= 63;
+						value = digit >> baseBits - offset;
 					}
 				}
 				if (value != 0) {
@@ -333,11 +345,11 @@ namespace plastic {
 				return;
 			}
 
-			::std::uint64_t log2Base{log2BaseTable[base]};
-			int chunkSize{chunkSizeTable[base]};
+			::std::uint64_t log{logTable[base]};
+			::std::size_t chunkSize{chunkSizeTable[base]};
 			::std::uint64_t chunkBase{chunkBaseTable[base]};
 
-			_data.resize((_mul(end - begin, log2Base).first >> 3) + 2);
+			_data.resize((_mul(end - begin, log).first >> 3) + 2);
 			auto dBegin{_data.begin()}, dEnd{dBegin};
 
 			//if (end - begin <= 2000) {
@@ -519,17 +531,91 @@ namespace plastic {
 			return _sign == 0 ? 0 : _sign > 0 ? _data.front() : ~_data.front() + 1;
 		}
 
-		friend ::std::string to_string(const bigint& num, int base = 0) {
+		friend ::std::string to_string(const bigint& num, unsigned base = 10) {
+			if (base < 2 || base > 36) {
+				::std::abort();
+			}
+
+			if (num._sign == 0) {
+				return "0";
+			}
+
+			::std::string str;
+
+			auto begin{num._data.begin()}, end{num._data.end()};
+
+			::std::size_t zeroBits{static_cast<::std::size_t>(::std::countl_zero(num._data.back()))};
+			::std::size_t bits{(num._data.size() << 6) - zeroBits};
+
+			static constexpr ::std::string_view toCharTable{"0123456789abcdefghijklmnopqrstuvwxyz"};
+
+			/*
+				Table[StringJoin["0x", IntegerString[If[IntegerQ[Log2[b]], Log2[b], Ceiling[2^64/Log2[b]]], 16, 16]], {b, 2, 36}]
+			*/
+
+			static constexpr ::std::array<::std::uint64_t, 37> logTable{
+				0x0000000000000000, 0x0000000000000000, 0x0000000000000001, 0xa1849cc1a9a9e94f,
+				0x0000000000000002, 0x6e40d1a4143dcb95, 0x6308c91b702a7cf5, 0x5b3064eb3aa6d389,
+				0x0000000000000003, 0x50c24e60d4d4f4a8, 0x4d104d427de7fbcd, 0x4a00270775914e89,
+				0x4768ce0d05818e13, 0x452e53e365907bdb, 0x433cfffb4b5aae56, 0x41867711b4f85356,
+				0x0000000000000004, 0x3ea16afd58b10967, 0x3d64598d154dc4df, 0x3c43c23018bb5564,
+				0x3b3b9a42873069c8, 0x3a4898f06cf41aca, 0x39680b13582e7c19, 0x3897b2b751ae561b,
+				0x37d5aed131f19c99, 0x372068d20a1ee5cb, 0x3676867e5d60de2a, 0x35d6deeb388df870,
+				0x354071d61c77fa2f, 0x34b260c5671b18ad, 0x342be986572b45cd, 0x33ac61b998fbbdf3,
+				0x0000000000000005, 0x32bfd90114c12862, 0x3251dcf6169e45f3, 0x31e8d59f180dc631,
+				0x3184648db8153e7b
+			};
+
+			if (::std::has_single_bit(base)) {
+				::std::size_t baseBits{logTable[base]};
+				::std::size_t mask{(::std::size_t{1} << baseBits) - 1};
+
+				str.resize(bits / baseBits + 2);
+				auto sBegin{str.begin()}, sEnd{sBegin};
+
+				if (num._sign < 0) {
+					*sEnd++ = '-';
+				}
+
+				::std::size_t mod{bits % baseBits};
+				::std::size_t offset{64 - zeroBits - (mod == 0 ? baseBits : mod)};
+				--end;
+				while (true) {
+					while (offset < 64) {
+						*sEnd++ = toCharTable[*end >> offset & mask];
+						offset -= baseBits;
+					}
+					if (begin == end) {
+						break;
+					}
+					::std::uint64_t high{*end << ~offset + 1 & mask};
+					offset &= 63;
+					*sEnd++ = toCharTable[high | *--end >> offset];
+					offset -= baseBits;
+				}
+
+				str.resize(sEnd - sBegin);
+
+				return str;
+			}
+
+			//if (num._data.size() <= 35) {
+			::std::uint64_t log{logTable[base]};
+
+			str.resize(_mul(bits, log).first + 3);
+			auto sBegin{str.begin()}, sEnd{sBegin};
+
+			if (num._sign < 0) {
+				*sEnd++ = '-';
+			}
+
+
+
+			return str;
+			//}
 
 		}
 
-		friend ::std::istream& operator>>(::std::istream& is, bigint& num) {
-
-		}
-
-		friend ::std::ostream& operator<<(::std::ostream& os, const bigint& num) {
-
-		}
 	};
 
 }
