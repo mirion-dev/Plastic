b48f5ab2b1e572630a43f97de021e6de7a3ca2a0
Mirion <51433180+MirionQs@users.noreply.github.com>
2024-10-09 15:10:08 +0800

    refactor: remove leading underlines of certain variables

--- list.h
+++ list.h
@@ -1,24 +1,26 @@
 #pragma once
 
+#include "utils.h"
+
 #include <iterator>
 
 namespace plastic {
 
 	template<class T>
 	class list {
-		struct _node {
-			T _value;
-			_node* _prev;
-			_node* _next;
+		struct node {
+			T value;
+			node* prev;
+			node* next;
 		};
 
-		_node* _sentinel;
+		node* _sentinel;
 
 	public:
 		class iterator {
-			friend list;
+			friend class list;
 
-			_node* _ptr;
+			node* _ptr;
 
 		public:
 			using difference_type = ptrdiff_t;
@@ -27,16 +29,16 @@ namespace plastic {
 			using reference = T&;
 			using iterator_category = std::bidirectional_iterator_tag;
 
-			explicit iterator(_node* node = nullptr) {
+			explicit iterator(node* node = nullptr) {
 				_ptr = node;
 			}
 
 			reference operator*() const {
-				return _ptr->_value;
+				return _ptr->value;
 			}
 
 			pointer operator->() const {
-				return &_ptr->_value;
+				return &_ptr->value;
 			}
 
 			bool operator==(iterator it) const {
@@ -44,7 +46,7 @@ namespace plastic {
 			}
 
 			iterator& operator++() {
-				_ptr = _ptr->_next;
+				_ptr = _ptr->next;
 				return *this;
 			}
 
@@ -55,7 +57,7 @@ namespace plastic {
 			}
 
 			iterator& operator--() {
-				_ptr = _ptr->_prev;
+				_ptr = _ptr->prev;
 				return *this;
 			}
 
@@ -66,18 +68,18 @@ namespace plastic {
 			}
 		};
 
-		explicit list(size_t count = 0, const T& value = {}) {
-			_sentinel = new _node;
-			_sentinel->_next = _sentinel->_prev = _sentinel;
+		explicit list(size_t count = {}, const T& value = {}) {
+			_sentinel = new node;
+			_sentinel->next = _sentinel->prev = _sentinel;
 			resize(count, value);
 		}
 
 		template<std::input_iterator iter>
 		explicit list(iter first, iter last) {
-			_sentinel = new _node;
-			_sentinel->_next = _sentinel->_prev = _sentinel;
+			_sentinel = new node;
+			_sentinel->next = _sentinel->prev = _sentinel;
 			while (first != last) {
-				_sentinel->_next = new _node{ *--last, _sentinel, _sentinel->_next };
+				_sentinel->next = new node{ *--last, _sentinel, _sentinel->next };
 			}
 		}
 
@@ -87,26 +89,26 @@ namespace plastic {
 		}
 
 		bool empty() const {
-			return _sentinel->_next == _sentinel;
+			return _sentinel->next == _sentinel;
 		}
 
 		void clear() const {
-			_node* i{ _sentinel->_next };
+			node* i{ _sentinel->next };
 			while (i != _sentinel) {
-				_node* next{ i->_next };
+				node* next{ i->next };
 				delete i;
 				i = next;
 			}
 		}
 
 		void resize(size_t count, const T& value = {}) const {
-			for (size_t i{ 0 }; i < count; ++i) {
-				_sentinel->_next = new _node{ value, _sentinel, _sentinel->_next };
+			for (size_t i{}; i != count; ++i) {
+				_sentinel->next = new node{ value, _sentinel, _sentinel->next };
 			}
 		}
 
 		iterator begin() const {
-			return iterator{ _sentinel->_next };
+			return iterator{ _sentinel->next };
 		}
 
 		iterator end() const {
@@ -114,94 +116,74 @@ namespace plastic {
 		}
 
 		T& front() const {
-#ifdef PLASTIC_VERIFY
-			if (empty()) {
-				std::abort();
-			}
-#endif
-			return _sentinel->_next->_value;
+			PLASTIC_VERIFY(!empty());
+			return _sentinel->next->value;
 		}
 
 		T& back() const {
-#ifdef PLASTIC_VERIFY
-			if (empty()) {
-				std::abort();
-			}
-#endif
-			return _sentinel->_prev->_value;
+			PLASTIC_VERIFY(!empty());
+			return _sentinel->prev->value;
 		}
 
 		void push_front(const T& value) const {
-			_sentinel->_next->_next->_prev = _sentinel->_next = new _node{ value, _sentinel, _sentinel->_next };
+			_sentinel->next->next->prev = _sentinel->next = new node{ value, _sentinel, _sentinel->next };
 		}
 
 		void pop_front() const {
-#ifdef PLASTIC_VERIFY
-			if (empty()) {
-				std::abort();
-			}
-#endif
-			_node* temp{ _sentinel->_next };
-			_sentinel->_next = temp->_next;
-			temp->_next->_prev = _sentinel;
+			PLASTIC_VERIFY(!empty());
+			node* temp{ _sentinel->next };
+			_sentinel->next = temp->next;
+			temp->next->prev = _sentinel;
 			delete temp;
 		}
 
 		void push_back(const T& value) const {
-			_sentinel->_prev->_prev->_next = _sentinel->_prev = new _node{ value, _sentinel->_prev, _sentinel };
+			_sentinel->prev->prev->next = _sentinel->prev = new node{ value, _sentinel->prev, _sentinel };
 		}
 
 		void pop_back() const {
-#ifdef PLASTIC_VERIFY
-			if (empty()) {
-				std::abort();
-			}
-#endif
-			_node* temp{ _sentinel->_prev };
-			_sentinel->_prev = temp->_prev;
-			temp->_prev->_next = _sentinel;
+			PLASTIC_VERIFY(!empty());
+			node* temp{ _sentinel->prev };
+			_sentinel->prev = temp->prev;
+			temp->prev->next = _sentinel;
 			delete temp;
 		}
 
 		iterator insert(iterator pos, const T& value, size_t count = 1) const {
-			_node* i{ pos._ptr };
-			for (size_t j{ 0 }; j < count; ++j) {
-				i = i->_prev = new _node{ value, i->_prev, i };
+			node* i{ pos._ptr };
+			for (size_t j{}; j != count; ++j) {
+				i = i->prev = new node{ value, i->prev, i };
 			}
-			i->_prev->_next = i;
+			i->prev->next = i;
 			return iterator{ i };
 		}
 
 		template<std::input_iterator iter>
 		iterator insert(iterator pos, iter first, iter last) const {
-			_node* i{ pos._ptr };
+			node* i{ pos._ptr };
 			while (first != last) {
-				i = i->_prev = new _node{ *first++, i->_prev, i };
+				i = i->prev = new node{ *first++, i->prev, i };
 			}
-			i->_prev->_next = i;
+			i->prev->next = i;
 			return iterator{ i };
 		}
 
 		iterator erase(iterator pos) const {
-			_node* i{ (++pos)._ptr };
-			_node* prev{ i->_prev };
-#ifdef PLASTIC_VERIFY
-			if (prev == _sentinel) {
-				std::abort();
-			}
-#endif
-			i->_prev = prev->_prev;
-			i->_prev->_next = i;
+			node* i{ (++pos)._ptr };
+			node* prev{ i->prev };
+			PLASTIC_VERIFY(prev != _sentinel);
+			i->prev = prev->prev;
+			i->prev->next = i;
 			delete prev;
 			return pos;
 		}
 
 		iterator erase(iterator first, iterator last) const {
-			_node* i{ first._ptr }, * j{ last._ptr };
-			i->_prev->_next = j;
-			j->_prev = i->_prev;
+			node* i{ first._ptr }, * j{ last._ptr };
+			i->prev->next = j;
+			j->prev = i->prev;
 			while (i != j) {
-				_node* next = i->_next;
+				node* next = i->next;
 				delete i;
 				i = next;
 			}
@@ -209,7 +191,7 @@ namespace plastic {
 		}
 
 		std::compare_three_way_result<T> operator<=>(const list& container) const {
-			iterator i{ _sentinel->_next }, j{ container._sentinel->_next };
+			iterator i{ _sentinel->next }, j{ container._sentinel->next };
 			while (i != _sentinel && j != container._sentinel) {
 				std::compare_three_way_result<T> cmp{ *i++ <=> *j++ };
 				if (cmp != 0) {
